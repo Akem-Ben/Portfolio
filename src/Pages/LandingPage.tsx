85e37e6aadaf6519ec76f7986e93aa839504c29c
Akem-Ben <akemini.ndaobong@gmail.com>
2024-04-23 22:09:45 +0100

    setup

---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import Navigation from "../Components/Navbar"
+
+
+const LandingPage = () => {
+
+    return (
+        <>
+        <Navigation />
+        </>
+    )
+}
+
+export default LandingPage
