e8b492e532ef12608ce52256d5264621ad387156
Akem-Ben <akemini.ndaobong@gmail.com>
2024-04-23 23:14:51 +0100

    navbar setup and preliminary designs

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,40 +1,92 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useState } from 'react';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from '../assets/images/Abn.jpeg'
+import { useState, useEffect } from "react";
+import { PiLinkedinLogoBold } from "react-icons/pi";
+import { TfiTwitter } from "react-icons/tfi";
+import './Navbar.css'
 
 const Navigation = () => {
-    const [activeLink, setActiveLink] = useState('home')
-    const [scrolled, setScrolled] = useState(false)
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
+  const onUpdateActiveLink = (value: string) => {
+    setActiveLink(value);
+  };
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return window.removeEventListener("scroll", onScroll);
+  }, [scrolled]);
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="logo" />
+          <img src={logo} alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className='navbar-toggle-icon'></span>
+          <span className="navbar-toggle-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
           </Nav>
-          <span className='navbar-text'>
+          <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={""} alt="" /></a>
-                <a href="#"><img src={""} alt="" /></a>
-                <a href="#"><img src={""} alt="" /></a>
+              <a href="#">
+              <PiLinkedinLogoBold style={{color: 'white', fontWeight: 900}} />
+              </a>
+              <a href="#">
+              <TfiTwitter style={{color: 'white', fontWeight: 900}} />
+              </a>
             </div>
-            <button className="contact-us" onClick={()=> console.log('contact us')}><span>Let's Connect</span></button>
+            <button
+              className="contact-us"
+              onClick={() => console.log("contact us")}
+            >
+              <span>Let's Connect</span>
+            </button>
           </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default Navigation;
